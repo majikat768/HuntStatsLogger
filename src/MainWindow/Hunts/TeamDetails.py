b55a0618c91e38f349839dd51b0b9235c9760d2a
Skerilyo <killian.clette@gmail.com>
2022-11-11 21:03:24 -0500

    Update TeamDetails.py

--- src/MainWindow/Hunts/TeamDetails.py
+++ src/MainWindow/Hunts/TeamDetails.py
@@ -122,7 +122,6 @@ class TeamDetails(QGroupBox):
                 hunterWidget.setLayout(hunterWidget.layout)
                 name = hunter['blood_line_name']
                 if name.lower() == settings.value("steam_name","").lower():
-
                     ownTeam = True
                 if isQp:
                     teamLabel.setText("%s<br>" % name)
